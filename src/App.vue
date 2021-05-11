207c68b19ec70d257c790c889c698be53c857ff8
Sebastian Schumacher <sebastian.schumacher@hotmail.de>
2021-05-11 13:37:46 +0200

    added socks image in data option

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="nav-bar"></div>
+      
+      <div class="product-display">
+        <div class="product-container">
+          <div class="product-image">
+            <a :href="url">
+              <img :src="image"> <!-- v-bind directive dynamically binding attribute to expression-->
+            </a>
+          </div> 
+          <div class="product-image">
+            <h1>{{ product }}</h1>
+          </div>
+        </div>
+      </div>
+  <!-- <HelloWorld msg="Welcome to My Vue.js App"/> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import socks_green from '@/assets/images/socks_green.jpg'
 
 export default {
   name: 'App',
+  data: function() { // data option
+        return {
+            product: 'Socks',
+            image: socks_green,
+            url: 'https://github.com/sscumac'
+            // <img alt="Vue logo" src="./assets/logo.png">
+        }
+  },
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped lang="css">
+  @import "./assets/styles.css"; 
 </style>
